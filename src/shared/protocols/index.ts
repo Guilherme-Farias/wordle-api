12d8e96ff5c8e742f1d29812af7d5e38f6fc0336
Guilherme Farias <guicfarias11@gmail.com>
2024-06-04 00:31:01 -0300

    chore: add Validation protocols

--- src/shared/protocols/index.ts
+++ src/shared/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from './controller';
 export * from './http';
 export * from './use-case';
+export * from './validation';
---
+++ src/shared/protocols/validation.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface IParamErrors {
+  [param: string]: string;
+}
+
+export interface IValidation<T = any> {
+  validate(params: T): void;
+}
