cfb8e20f3e2297e87eadf0ecba7eeef6acd8f45f
Guilherme Farias <guicfarias11@gmail.com>
2024-06-05 03:36:21 -0300

    feat: update AddWordUseCase

--- tests/modules/word/use-cases/add-word.use-case.spec.ts
+++ tests/modules/word/use-cases/add-word.use-case.spec.ts
@@ -1,7 +1,8 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { MockProxy, mock } from 'vitest-mock-extended';
 
-import { Word } from '@/modules/word/models';
+import { WordDateAlreadyExistisError } from '@/modules/word/errors';
+import { WordId, WordString } from '@/modules/word/models';
 import { WordRepository } from '@/modules/word/repositories';
 import { AddWordUseCase } from '@/modules/word/use-cases';
 import { makeAddWordUseCaseDTO } from '@tests/helpers/mocks';
@@ -11,16 +12,16 @@ describe('AddWordUseCase', () => {
   let sut: AddWordUseCase;
   let wordRepositoryMock: MockProxy<WordRepository>;
 
+  const word = {
+    id: 'valid_id' as WordId,
+    word: dto.word as WordString,
+    date: dto.date,
+  } as const;
+
   const makeAppointmentRepositoryMock = () => {
     wordRepositoryMock = mock<WordRepository>();
-
-    wordRepositoryMock.create.mockReturnValue(
-      Promise.resolve({
-        id: 'valid_id',
-        word: dto.word,
-        date: dto.date,
-      } as Word),
-    );
+    wordRepositoryMock.findByDate.mockReturnValue(Promise.resolve(null));
+    wordRepositoryMock.create.mockReturnValue(Promise.resolve(word));
     return wordRepositoryMock;
   };
 
@@ -28,18 +29,32 @@ describe('AddWordUseCase', () => {
     sut = new AddWordUseCase(makeAppointmentRepositoryMock());
   });
 
+  it('should call WordRepository.findByDate with available date', async () => {
+    await sut.execute(dto);
+    expect(wordRepositoryMock.findByDate).toHaveBeenCalledWith(dto.date);
+    expect(wordRepositoryMock.findByDate).toReturnWith(null);
+  });
+
+  it('should throw WordDateAlreadyExistisError if WordRepository.findByDate return word', async () => {
+    wordRepositoryMock.findByDate.mockReturnValueOnce(Promise.resolve(word));
+    const promise = sut.execute(dto);
+    await expect(promise).rejects.toThrowError(WordDateAlreadyExistisError);
+  });
+
   it('should call WordRepository.create with correct values', async () => {
     await sut.execute(dto);
     expect(wordRepositoryMock.create).toHaveBeenCalledWith(dto);
-    expect(wordRepositoryMock.create).toReturnWith({
-      id: 'valid_id',
-      ...dto,
-    });
+    expect(wordRepositoryMock.create).toReturnWith(word);
   });
 
   it('should rethrow if WordRepository.create throws', async () => {
     const error = new Error('create_error');
     wordRepositoryMock.create.mockRejectedValueOnce(error);
     await expect(sut.execute(dto)).rejects.toThrow(error);
+  });
+
+  it('should return Word if valid data is provided', async () => {
+    const returnedWord = await sut.execute(dto);
+    expect(returnedWord).toEqual(word);
   });
 });
