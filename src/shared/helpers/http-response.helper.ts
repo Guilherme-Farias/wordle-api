d6012e257bec5faea32f22ba417d9b70d65e51b8
Guilherme Farias <guicfarias11@gmail.com>
2024-06-05 19:54:57 -0300

    feat: add responses in HttpResponseHelper

--- src/shared/helpers/http-response.helper.ts
+++ src/shared/helpers/http-response.helper.ts
@@ -9,8 +9,16 @@ type BadRequestProps = HttpErrorProps & {
 };
 
 export class HttpResponseHelper {
+  public static ok<T>(data: T): IHttpResponse<T> {
+    return { statusCode: 200, body: data };
+  }
+
   public static created<T>(data: T): IHttpResponse<T> {
     return { statusCode: 201, body: data };
+  }
+
+  public static noContent(): IHttpResponse {
+    return { statusCode: 204, body: null };
   }
 
   public static badRequest(props?: BadRequestProps): IHttpResponse {
@@ -18,9 +26,15 @@ export class HttpResponseHelper {
     const params = props?.params ?? undefined;
     return { statusCode: 400, body: { error, params } };
   }
+
   public static forbidden(props?: HttpErrorProps): IHttpResponse {
     const error = props?.error ?? 'Forbidden';
     return { statusCode: 403, body: { error } };
+  }
+
+  public static notFound(props?: HttpErrorProps): IHttpResponse {
+    const error = props?.error ?? 'Not Found';
+    return { statusCode: 404, body: { error } };
   }
 
   public static internalServerError(props?: HttpErrorProps): IHttpResponse {
