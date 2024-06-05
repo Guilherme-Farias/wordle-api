ac0b93087b1917cd9a192fb077f5f4744e14ec17
Guilherme Farias <guicfarias11@gmail.com>
2024-06-04 22:29:30 -0300

    chore: add HttpResponseHelper

---
+++ src/shared/helpers/http-response.helper.ts
@@ -0,0 +1,26 @@
+import { IHttpResponse, IParamErrors } from '../protocols';
+
+type HttpErrorProps = {
+  error?: string;
+};
+
+type BadRequestProps = HttpErrorProps & {
+  params?: IParamErrors;
+};
+
+export class HttpResponseHelper {
+  public static created<T>(data: T): IHttpResponse<T> {
+    return { statusCode: 201, body: data };
+  }
+
+  public static badRequest(props?: BadRequestProps): IHttpResponse {
+    const error = props?.error ?? 'Bad Request';
+    const params = props?.params ?? undefined;
+    return { statusCode: 400, body: { error, params } };
+  }
+
+  public static internalServerError(props?: HttpErrorProps): IHttpResponse {
+    const error = props?.error ?? 'Internal Server Error';
+    return { statusCode: 500, body: { error } };
+  }
+}
--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,1 +1,2 @@
+export * from './http-response.helper';
 export * from './zod-helper';
