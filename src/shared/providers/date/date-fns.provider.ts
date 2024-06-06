0c1610c3ddeed834d35cd787d6735a84467a1e5d
Guilherme Farias <guicfarias11@gmail.com>
2024-06-06 13:05:24 -0300

    feat: add startOfDay in DateProvider

--- src/shared/providers/date/date-fns.provider.ts
+++ src/shared/providers/date/date-fns.provider.ts
@@ -1,9 +1,13 @@
-import { parseISO } from 'date-fns';
+import { parseISO, startOfDay } from 'date-fns';
 
 import { DateProvider } from './date.provider';
 
 export class DateFnsProvider implements DateProvider {
-  parseISO(unformattedDate: string): Date {
-    return parseISO(unformattedDate);
+  parseISO(isoString: string): Date {
+    return parseISO(isoString);
+  }
+
+  startOfDay(value: string | number | Date): Date {
+    return startOfDay(value);
   }
 }
--- tests/shared/providers/date/date-fns.provider.spec.ts
+++ tests/shared/providers/date/date-fns.provider.spec.ts
@@ -8,11 +8,16 @@ vi.mock('date-fns', () => ({
   parseISO(): Date {
     return 'parse_iso' as unknown as Date;
   },
+  startOfDay(): Date {
+    return 'start_of_day' as unknown as Date;
+  },
 }));
 
 describe('DateFnsProvider', () => {
   let sut: DateFnsProvider;
-  const date = faker.date.soon().toISOString();
+  const date = faker.date.soon();
+  const dateString = date.toISOString();
+  const dateNumber = date.getTime();
 
   beforeEach(() => {
     sut = new DateFnsProvider();
@@ -21,12 +26,12 @@ describe('DateFnsProvider', () => {
   describe('parseISO()', () => {
     it('should call parseISO with correct values', () => {
       const parseISOSpy = vi.spyOn(dateFns, 'parseISO');
-      sut.parseISO(date);
-      expect(parseISOSpy).toHaveBeenCalledWith(date);
+      sut.parseISO(dateString);
+      expect(parseISOSpy).toHaveBeenCalledWith(dateString);
     });
 
     it('should return a parsed date on success', () => {
-      const parseISO = sut.parseISO(date);
+      const parseISO = sut.parseISO(dateString);
       expect(parseISO).toBe('parse_iso');
     });
 
@@ -34,8 +39,44 @@ describe('DateFnsProvider', () => {
       vi.spyOn(dateFns, 'parseISO').mockImplementationOnce(
         throwError('parseISO_error'),
       );
-      expect(() => sut.parseISO(date)).toThrowErrorMatchingInlineSnapshot(
+      expect(() => sut.parseISO(dateString)).toThrowErrorMatchingInlineSnapshot(
         `[Error: parseISO_error]`,
+      );
+    });
+  });
+
+  describe('startOfDay()', () => {
+    it('should call startOfDay with correct values', () => {
+      const startOfDaySpy = vi.spyOn(dateFns, 'startOfDay');
+      sut.startOfDay(date);
+      expect(startOfDaySpy).toHaveBeenCalledWith(date);
+      sut.startOfDay(dateString);
+      expect(startOfDaySpy).toHaveBeenCalledWith(dateString);
+      sut.startOfDay(dateNumber);
+      expect(startOfDaySpy).toHaveBeenCalledWith(dateNumber);
+    });
+
+    it('should return the start of the day for a given date', () => {
+      const startOfDay = sut.startOfDay(date);
+      expect(startOfDay).toBe('start_of_day');
+    });
+
+    it('should return the start of the day for a given string', () => {
+      const startOfDay = sut.startOfDay(dateString);
+      expect(startOfDay).toBe('start_of_day');
+    });
+
+    it('should return the start of the day for a given number', () => {
+      const startOfDay = sut.startOfDay(dateString);
+      expect(startOfDay).toBe('start_of_day');
+    });
+
+    it('should throw if startOfDay throws', () => {
+      vi.spyOn(dateFns, 'startOfDay').mockImplementationOnce(
+        throwError('startOfDay_error'),
+      );
+      expect(() => sut.startOfDay(date)).toThrowErrorMatchingInlineSnapshot(
+        `[Error: startOfDay_error]`,
       );
     });
   });
