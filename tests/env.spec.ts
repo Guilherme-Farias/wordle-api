206a65ebd929edda86fc27ca14278f95cb7bc977
Guilherme Farias <guicfarias11@gmail.com>
2024-06-06 04:47:28 -0300

    feat: add DATABASE_URL in .env validator

--- tests/env.spec.ts
+++ tests/env.spec.ts
@@ -1,7 +1,34 @@
 import { env } from '@/env';
 
 describe('env', () => {
-  it('should be .env.test', () => {
-    expect(env).toHaveProperty('NODE_ENV', 'test');
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    vi.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
+  it('should throws when invalid .env is provided', async () => {
+    process.env.NODE_ENV = 'invalid_env';
+    try {
+      vi.spyOn(console, 'error').mockImplementationOnce(vi.fn());
+      await import('@/env');
+      assert.fail();
+    } catch (error) {
+      expect(error).instanceOf(Error);
+      expect((error as Error).message).toMatchInlineSnapshot(
+        `"Invalid environment variables."`,
+      );
+    }
+  });
+
+  it('should return .env with valid data', () => {
+    expect(env).toHaveProperty('NODE_ENV');
+    expect(env).toHaveProperty('PORT');
+    expect(env).toHaveProperty('DATABASE_URL');
   });
 });
